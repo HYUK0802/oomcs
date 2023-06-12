5f5acdb4dbac19e16c8bf8505a4b3dc6b9d2f8e8
hyuk <tig05205@naver.com>
2023-06-12 15:40:35 +0900

    first

---
+++ src/test/java/com/jhkim/mypage/MypageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhkim.mypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
